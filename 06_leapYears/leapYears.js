f0aba5d907e013ab6d4d4a7e206dbcb176c1c537
antferraiolo <54289964+antferraiolo@users.noreply.github.com>
2023-03-30 23:38:33 -0400

    Completed leapYears function
    Used series of if else statements to determine if the input year was a leap year. I want to study more how the solution looks comparatively.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR"
+    let modulosFour = year % 4;
+    let modulos100 = year % 100;
+    let modulos400 = year % 400;
 
+    if (modulosFour == 0){
+        if(modulos100 == 0){
+            if(modulos400 == 0 ){
+                return true;
+            }
+            return false; 
+        }
+        return true; 
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
