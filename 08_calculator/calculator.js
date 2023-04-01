55e792861164f77a583320b8150e57def74e725b
antferraiolo <54289964+antferraiolo@users.noreply.github.com>
2023-04-01 15:30:41 -0400

    Completed math functions
    Searched for more information regarding factorial and array. Need to learn array functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
+const add = function(number1,number2) {
+	return number1 + number2; 
+};
+
+const subtract = function(number1,number2) {
+	return number1 - number2;
+};
+
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
+};
+
+const multiply = function(array) {
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
+};
+
+const power = function(num1, num2) {
+  return num1 ** num2;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
