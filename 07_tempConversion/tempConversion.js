4e02cc075186e371c1bead7f4e0fe5686345ec6b
antferraiolo <54289964+antferraiolo@users.noreply.github.com>
2023-04-01 14:55:58 -0400

    Completed convert functions
    Used round function from online to round to one decimal place. Seen on line 6 in JS.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+ // if (!Number.isInteger(number)) return "ERROR";
+  let convertednumber = 0; 
+  var multiplier = Math.pow(10, 1 || 0);
+  convertednumber = ((number - 32)* (5/9));
+  convertednumber = Math.round(convertednumber * multiplier)/multiplier;
+  return convertednumber;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+//x °C ≘ (x × 9/5+ 32) °F
+// if (!Number.isInteger(number)) return "ERROR";
+let convertednumber = 0; 
+var multiplier = Math.pow(10, 1 || 0);
+convertednumber = (number * (9/5) + 32);
+convertednumber = Math.round(convertednumber * multiplier)/multiplier;
+return convertednumber;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
